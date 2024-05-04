0b87bb9f50d09c552553591736de4716ea7937b5
FauzanRizkiAfri <163797425+FauzanRizkiAfri@users.noreply.github.com>
2024-05-05 06:08:30 +0700

    4

--- 0129_Doublelinkedlist/0129_Doublelinkedlist.cpp
+++ 0129_Doublelinkedlist/0129_Doublelinkedlist.cpp
@@ -32,4 +32,21 @@ void addNode() {
 		newNode->prev = NULL;
 		START = newNode;
 	}
-	
+	else {
+		Node* current = START;
+		Node* previous = NULL;
+
+		while (current != NULL && current->noMhs < newNode->noMhs)
+		{
+			previous = current;
+			current = current->next;
+		}
+		newNode->next = current;
+		newNode->prev = previous;
+
+		if (current != NULL)
+		{
+			current->prev = newNode;
+		}
+
+		

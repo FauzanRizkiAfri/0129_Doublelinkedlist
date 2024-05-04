dc02c781a5296be7a586e91002b865c005fdb57f
FauzanRizkiAfri <163797425+FauzanRizkiAfri@users.noreply.github.com>
2024-05-05 06:20:11 +0700

    19

--- 0129_Doublelinkedlist/0129_Doublelinkedlist.cpp
+++ 0129_Doublelinkedlist/0129_Doublelinkedlist.cpp
@@ -197,4 +197,12 @@ int main()
 			cout << "5. Search for a record in the list" << endl;
 			cout << "6.Exit" << endl;
 			cout << "\nEnter your choice (1-6): ";
-		
+			char ch;
+			cin >> ch;
+
+			switch (ch)
+			{
+			case '1':
+				addNode();
+				break;
+	
